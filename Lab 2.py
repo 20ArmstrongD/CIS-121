5011f3173a3ecc49599c0f1f124da9c4dd71e1ff
20ArmstrongD <you@example.com>
2022-09-01 13:29:09 -0500

    created lab document

---
+++ Lab 2.py
@@ -0,0 +1,45 @@
+from doctest import DONT_ACCEPT_BLANKLINE
+
+
+dog_years = 7
+
+#asking user for age/converting input str to int
+
+#create a vaiable to store humans age 
+human_age = 20
+
+human_in_dog_years = dog_years * human_age
+# +, -, *
+#create a variable for human age in dog years
+print(dog_years * 20)
+#calculate how old is the human in dog years
+print("an age",human_age,"human should be",dog_years *20, "in dogs age")
+# calculate how old is the human in dog years
+human_age = int(input("How old are you?")) 
+
+#create a script that asks the user for name, lastname, and age. print out info as follows
+#hi Donnie Armstrong, you are 20 years old
+
+name = input("please enter your name: ")
+lastname = input("please enter your last name: ")
+age = input("please enter your age: ")
+
+print("Hi",name,lastname,"you are",age, "years old")
+
+
+#horse years part
+human_age = float(input("what is your age?"))
+
+horse_years = 3 * ((((human_age**2 - 47)/7))+12)
+
+print("an age",human_age, "human should be about",horse_years, "in horse age")
+
+
+# to calculate a humans height you do the following: age^2
+
+#print out the huamns age and estimated height
+
+humanage = float(input("what is your age?"))
+humanheight = humanage **2
+
+print("a human with an age of ", humanage, "should be", humanheight "feet tall")
--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # CIS-121
 
 ### Donald
+
+
 ### steps to follow
 # git add .
 # git commit-m "message goes here"
 # git push origin
 
-this is hard
 
 
 
