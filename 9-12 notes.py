0450cb0da429093a1ebcf4f28d9a9088f14178e0
20ArmstrongD <you@example.com>
2022-09-15 13:49:46 -0500

    added comment

---
+++ 9-12 notes.py
@@ -0,0 +1,26 @@
+
+x = int(input("please enter a number between 35-1000: "))
+
+if 35 <= x <= 1000:
+    if x >= 100: 
+        print(100)
+    else:
+        while x <= 100: 
+         if x % 2 == 0:
+            print(x)
+            x+=1 
+
+else:
+        print("you did not enter a valid number :( ")     
+
+
+#boolean
+winner = True
+Loser = False   
+
+
+
+
+
+
+
