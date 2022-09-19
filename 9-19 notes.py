ff3bb1f07844cbf767fd801d1d58210f6cf917ba
20ArmstrongD <you@example.com>
2022-09-19 15:01:11 -0500

    made code run for previous porblems

--- 9-19 notes.py
+++ 9-19 notes.py
@@ -1,9 +1,9 @@
 # while loop:
 # use when you have condition like stoping at a certain point
-# number = 0 
-# while number <= 10:
-#     print(number)
-#     number +=1
+number = 0 
+while number <= 10:
+    print(number)
+    number +=1
 
 #number % 2 == 0: is for finding even number
 #number % 2 == 1: is for finding if number is odd
@@ -12,35 +12,36 @@
 # # for or i are the same
 # # use for beggining to end 
 # # you can use for and while loops for same results
-# for number in range(1,11):
-#     print(number)
+for number in range(1,11):
+    print(number)
 
-# for number in range(1,11):
-#     if number % 2 == 0:
-#         print(number)
+#prints out number within range that are even
+for number in range(1,11):
+    if number % 2 == 0:
+        print(number)
 
 #function 
 #def means define
 #when writing a function lowercase the first letter of the word then captialize the first leter of every word
 #scope
-# def askUserForName():
-#     name = input("please enter your name: ")
-#     return name
-# name = askUserForName()
-# print(name)
-# name2 = askUserForName()
-# print(name2)
+def askUserForName():
+    name = input("please enter your name: ")
+    return name
+name = askUserForName()
+print(name)
+name2 = askUserForName()
+print(name2)
 
 #create a function that asks the user for their age. then use that value to priont out the next 20 years (use a for loop)
 
-# def askUserForAge():
-#     age = int(input("please enter your age"))
-#     return age
+def askUserForAge():
+    age = int(input("please enter your age"))
+    return age
 
-# age = askUserForAge()
+age = askUserForAge()
 
-# for number in range(age,age+21):
-#      print(number)
+for number in range(age,age+21):
+     print(number)
 
 
 #string Manipulation
@@ -75,7 +76,7 @@ if summ >= 30:
 # if user ==1:
 #     print(user, user * 2, user * 3, user * 4, user * 5)
 
-# thes 2 for loops allow me to go level by level
+# this 2 for loops allow me to go level by level
 # for i in range(1,7): #level manager
 #     for j in range(1,i): #
 #         print(j, end="")
