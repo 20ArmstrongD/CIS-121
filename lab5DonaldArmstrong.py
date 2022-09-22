6fa918b8a5a59fe4dbfed82746c2ac28b47cf9e3
20ArmstrongD <you@example.com>
2022-09-22 13:28:16 -0500

    added message

---
+++ lab5DonaldArmstrong.py
@@ -0,0 +1,98 @@
+
+# I have no idea how to solves this or even how i should go about solving this problem, cengage doesn't do anything to really show me how to solve or understand the concepts.
+
+
+# # from re import L, T
+
+
+# def main():
+# #     def 
+# #     for i in letter:
+# #         L = T
+# #         T = L
+# #         A = 8
+# #         A = B
+# #         E = A
+# #         B = E
+
+    
+
+    
+
+	
+# encodedWord = "WBLARF8TTS"
+# encodedWord = "L8KAOUL"
+# encodedWord = "E8N8N8"
+# encodedWord = "8TRA8DY T8LA"
+# encodedWord = "8TT LHA TILLTA LIMAS"	
+# encodedWord = "LHA GRAAN FIATD GTA8MS IN LHA W8RM SUNEABMS"
+# encodedWord = "TONG T8E T8CKS L8SLY L8CO LIMA 8L TA8SL T8LATY"
+	
+	
+# 	# encodeword[2]
+
+# 	#encodedWord = "UUHO"  		#Used for Bonus
+# 	#encodedWord = "EOUUUUOUU" 	#Used for Bonus
+	
+# print(DecodeWord(encodedWord))
+
+
+
+
+
+#Your code goes here.
+
+
+
+def DecodeWord():
+        def main(): 
+
+    encodedWord = "WBLARF8TTS"
+    encodedWord = "L8KAOUL"
+    encodedWord = "E8N8N8"
+    encodedWord = "8TRA8DY T8LA"
+    encodedWord = "8TT LHA TILLTA LIMAS"	
+    encodedWord = "LHA GRAAN FIATD GTA8MS IN LHA W8RM SUNEABMS"
+    encodedWord = "TONG T8E T8CKS L8SLY L8CO LIMA 8L TA8SL T8LATY"
+    return DecodeWord
+
+for letter in DecodeWord:
+    if DecodeWord == T:
+        print(L)
+
+#    d={"L":"T","T":"L","8":"A","B":"A","A":"E","E":"B"} 
+
+#    ans = ""
+#    for i in s:
+#         if i in d:
+#             ans = ans + d[i]
+#         else:
+#             ans = ans + i
+#         return ans
+# print(decode("WBLARF8TTS"))
+# print(decode("L8KAOUL"))
+# print(decode("E8N8N8"))
+# print(decode("8TRA8DY T8LA"))
+# print(deocde("8TT LHA TILLTA LIMAS"))
+# print(decode("LHA GRAAN FIATD GTA8MS IN LHA W8RM SUNEABMS"))
+# print(decode("TONG T8E T8CKS L8SLY L8CO LIMA 8L TA8SL T8LATY"))
+
+    
+    
+
+
+
+	
+
+
+
+
+#This code triggers the main to run
+#we'll talk about this more in chapters 6,7, & 8.	
+    # if __name__ == "__main__":
+	#     main()
+
+
+
+
+
