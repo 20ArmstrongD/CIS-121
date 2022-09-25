66e2dd260da2ea18905810893fe7d5dbf5e8f347
20ArmstrongD <you@example.com>
2022-09-24 21:05:54 -0500

    made changers to fix problems

--- Exam1DonaldArmstrong.py.py
+++ Exam1DonaldArmstrong.py.py
@@ -1,10 +1,12 @@
 #1A doesnt work because random_number is a string
+#1a corrected age may or not be a numerical value
 #1B doesn't work because at that point in the code number is not defined
-#1c
-#1d
-#1e code runns forever shound ad print user number along with end
+#1c it will un because number is defined within the code nippet
+#1d will not run because print is not propley indented
+#1e this is correct
 # name = input("please enter your name:")
 # age = input ("please enter your age: ")
+
 
 # random_number =  10 /(age%23)
 
@@ -30,61 +32,97 @@
 
 
 #problem 2
-# milkcost = 2.00
-# eggscost = 1.50
-# baconbost = 3.00
-# milk =  float(input("how much milk dod you buy?:"))
-# eggs = float(input("how much eggs did you buy?:"))
-# bacon = float(input("how much bacon did you buy?:"))
+milkcost = 2.00
+eggscost = 1.50
+baconbost = 3.00
+milk =  float(input("how much milk dod you buy?:"))
+eggs = float(input("how much eggs did you buy?:"))
+bacon = float(input("how much bacon did you buy?:"))
 
-# total_cost = milk * milkcost + eggs * eggscost + bacon * baconbost
-# print("you bought", milk, "milks,", eggs, "eggs, and", bacon, "bacons.")
-# print("your total is $", total_cost)
+subtotal = milk * milkcost + eggs * eggscost + bacon * baconbost
+taxamt = subtotal * 0.11
+total = subtotal + taxamt
+print("you bought", milk, "milks,", eggs, "eggs, and", bacon, "bacon" )
+print("your subtatal is",subtotal)
+print("your tax", taxamt)
+print("your total is $", total)
 
 #problem 3
 
-# def askUserForPhone():
-#     number = input("please enter your phone number ")
-#     return number
+# import numbers
+# from random import random
+
+
+# def formatphone(number): 
+#     # validate your input
+#     strnum = str(number) #converts input of number into a string
+#     if len(strnum) != 10: 
+#         print("give me number 10 digits long")
+#         return
+
+#     areacode = strnum[0:3] #for lines 58-60 splices or specifiys what numbers it takes
+#     firstpart = strnum[3:6]
+#     secondpart = strnum[6:10]
+
+
+#     print("(" + areacode + ") "+ firstpart +"-"+ secondpart) 
+
+# formatphone("7875551212")
+# formatphone(7875551212)
+
+
+    # numbe = str(input("please enter your phone number "))
+    # return number
 # number = askUserForPhone()
 # print(number)
 # number2 = askUserForPhone()
 # print(number2)
 
-phone_number = 7875551212
-print("(787)-555-1212")
+# phone_number = 7875551212
+# print("(787)-555-1212")
 
 # #for number in phone_number:
 #     print(number)
 
 #problem 4
-def askUserForNumber():
-    number = int(input("choose a number:"))
-    return number
+def randnums(): #definition for function
+    
+    for i in range (1,60): #assisns a range for random number(i)
+        if (48 % i == 0): #test if i is divisor of 48
+           
+            if i % 2 == 0: #test if i is even
+                print(i)
+            if i >= 15: # test if i numbers are greater that 15 
+                print("I generated the number", i, ", randomly")
+    
+# randnums() 
 
-number = askUserForNumber()
+# def askUserForNumber():
+#     number = int(input("choose a number:"))
+#     return number
 
-for number in range (1,60):
-    print(number)
+# number = askUserForNumber()
+# for number in range (1,60):
+#     print(number)
 
-while(number,60):
+# while(number,60):
 
-    if number <= 15:
-        print("i generated the number", number,", randomly")
+#     if number <= 15:
+#         print("i generated the number", number,", randomly")
 
-    elif number % 2 ==0:
-        print("this is an even number")
+# #     elif number % 2 ==0:
+# #         print("this is an even number")
 
 
-w = 20
-n = int(input("enter an upper bound for a check: "))
-print("between 1 and",n, "there are")  
+# # w = 20
+# # n = int(input("enter an upper bound for a check: "))
+# # print("between 1 and",n, "there are")  
 
-while (w,n):
-    while (n,w):
-        if n % w ==0:
-            print(n," not proper devisor")
-            n+=1
+# # while (w,n):
+# #     while (n,w):
+# #         if n % w ==0:
+# #             print(n," not proper devisor")
+#             n+=1
 
 #did my best with these last couple problem but really not understadning how to do them and havn't had the time for mavpass
 
